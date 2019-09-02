06b3db0a04901f8f21c35ff067ad863de1c954bb
Makss95 <54803452+Makss95@users.noreply.github.com>
2019-09-02 17:13:54 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Main page</title>
+    <link href="style.css" rel="stylesheet">
+    <h1>Apple shop</h1>
+</head>
+
+<div class="header">
+   <img class="logo" src="image/apple8.0.jpg" alt="logo" width="300" height="200">
+    <ul class="link">
+        <li><a class="link" href="index.html">Главная</a></li>
+        <li><a class="link" href="catalog.html">Каталог</a></li>
+        <li><a class="link" href="contacts.html">Контакты</a></li>
+    </ul>
+    </div>
+    <hr>
+    <body>
+    <div class="container">
+      
+    </div>
+	<?php
+$a=10;
+$b=26;
+list($a,$b)=[$b,$a];
+var_dump($a, $b);
+?>
+    <p>Приветствуем вас в самом большом Интернет-магазине Рунета!<br>
+    Приглашаем вас войти в зону комфортных и выгодных онлайн-покупок:
+    <ol>
+        <li>Воспользуйтесь удобным поиском товаров по разделам</li>
+        <li>Обратите внимание на текущие бестселлеры продаж</li>
+        <li>Сравните несколько товаров перед покупкой</li>
+         <li>Оплачивайте покупки любым удобным способом</li>
+        <li>Получайте товары по месту жительства: доставка курьером или почтой</li>
+    </ol>
+    </p>
+</body>
+<hr>
+<div class="footer">
+    <h6>Все права защищены &copy;</h6>
+</div>
+</html>
